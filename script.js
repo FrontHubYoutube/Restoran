f40e48a46ac0eccb913b3f0fc73c931a8fed072e
FrontHubYoutube <tapeshko4@gmail.com>
2024-11-21 22:47:30 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-const menu = document.querySelector('#menu-bars');
-const navbar = document.querySelector('.navbar');
-const searchIcon = document.querySelector('#search-icon')
-const searchForm = document.querySelector('#search-form')
-const closeForm = document.querySelector('#close')
+const menu = document.querySelector("#menu-bars");
+const navbar = document.querySelector(".navbar");
+const searchIcon = document.querySelector("#search-icon");
+const searchForm = document.querySelector("#search-form");
+const closeForm = document.querySelector("#close");
+const section = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("header .navbar a");
 
 menu.onclick = () => {
-    menu.classList.toggle('fa-x');
-    navbar.classList.toggle('active');
-}
+  menu.classList.toggle("fa-x");
+  navbar.classList.toggle("active");
+};
 searchIcon.onclick = () => {
-    searchForm.classList.toggle('active')
-    navbar.classList.remove('active');
-    menu.classList.remove('fa-x');
-}
+  searchForm.classList.toggle("active");
+  navbar.classList.remove("active");
+  menu.classList.remove("fa-x");
+};
 window.onscroll = () => {
-    menu.classList.remove('fa-x');
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active');
-      section.forEach((sec) => {
+  menu.classList.remove("fa-x");
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
+  section.forEach((sec) => {
     let top = window.scrollY;
     let height = sec.offsetHeight;
-    let offset = sec.offsetTop - 150;
+    let offset = sec.offsetTop - 50;
     let id = sec.getAttribute("id");
     if (top < offset + height && top > offset) {
       navLinks.forEach((links) => {
@@ -31,40 +33,41 @@ window.onscroll = () => {
       });
     }
   });
-}
+};
+
 closeForm.onclick = () => {
-    searchForm.classList.remove('active')
-}
+  searchForm.classList.remove("active");
+};
 
 var swiper = new Swiper(".home-slider", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+});
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-    loop:true,
-  });
-  var swiper = new Swiper(".review-slider", {
-    spaceBetween: 20,
-    centeredSlides: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
+    640: {
+      slidesPerView: 2,
     },
-    loop:true,
-    breakpoints: {
-        0: {
-            slidesPerView:1,
-        },
-        640: {
-            slidesPerView:2,
-        },
-        768: {
-            slidesPerView:2,
-        },
-        1024: {
-            slidesPerView:2.5,
-        },
+    768: {
+      slidesPerView: 2,
     },
-  });
+    1024: {
+      slidesPerView: 2.5,
+    },
+  },
+});
